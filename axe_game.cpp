a62c08159ae0f2a7d78ea01c15c42d5b13bbff0b
Flushwhy <cars6155@gmail.com>
2024-01-29 03:02:20 -0500

    Removed Extern C and updated C_cpp_properties

--- axe_game.cpp
+++ axe_game.cpp
@@ -1,52 +1,64 @@
 #include "raylib.h"
 
-extern "C" {
-    int main() 
+int main() 
+{
+    int width            = 800;
+    int height           = 450;
+    InitWindow( width, height, "Flush Was Here");
+
+    // Circle aka Player
+    int circle_radius{25};
+    int circle_x{200};
+    int circle_y{200};
+
+    // Circle Collision Detection EDGES
+    int left_circle_x   {circle_x - circle_radius};
+    int right_circle_x  {circle_x + circle_radius};
+    int upper_circle_y  {circle_y - circle_radius};
+    int bottom_circle_y {circle_y + circle_radius};
+
+    // speed for things
+    int direction{10}; // Axe
+    int player_speed{10}; // Circle
+
+    // Axe
+    int axe_length{50};
+    int axe_x{400};
+    int axe_y{0};
+
+    // Axe Collision Detection EDGES
+    int left_axe_x   {axe_x};
+    int right_axe_x  {axe_x + axe_length};
+    int upper_axe_y  {axe_y};
+    int bottom_axe_y {axe_x + axe_length};        
+
+    SetTargetFPS(60);
+    while (WindowShouldClose() == false)
     {
-        int width            = 800;
-        int height           = 450;
-        InitWindow( width, height, "Flush Was Here");
 
 
-        int circle_x{200};
-        int circle_y{200};
+        BeginDrawing();
+        ClearBackground(WHITE);
 
-        // speed for things
-        int direction{10}; // Axe
-        int player_speed{10}; // Circle
+        // Game Logic 
+        DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+        DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
 
-
-        int axe_x{400};
-        int axe_y{0};
-
-
-        SetTargetFPS(60);
-        while (WindowShouldClose() == false)
+        axe_y += direction;
+        if (axe_y > height || axe_y < width)
         {
-            BeginDrawing();
-            ClearBackground(WHITE);
-
-            // Game Logic 
-            DrawCircle(circle_x, circle_y, 25, BLUE);
-            DrawRectangle(axe_x, axe_y, 50, 50, RED);
-
-            axe_y += direction;
-            if (axe_y > height || axe_y < 0)
-            {
-                direction = -direction;
-            }
-
-            if (IsKeyDown(KEY_D) && circle_x < 350)
-            {
-                circle_x += player_speed;
-            }
-            if (IsKeyDown(KEY_A) && circle_x > 0)
-            {
-                circle_x -= player_speed;
-            }
-
-
-            EndDrawing();
+            direction = -direction;
         }
+
+        if (IsKeyDown(KEY_D) && circle_x < 350)
+        {
+            circle_x += player_speed;
+        }
+        if (IsKeyDown(KEY_A) && circle_x > 0)
+        {
+            circle_x -= player_speed;
+        }
+        
+        EndDrawing();
+       }
     }
-}
